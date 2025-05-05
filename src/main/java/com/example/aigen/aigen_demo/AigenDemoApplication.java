041ea88cda9ddc6931d12a48ce213a117a51ad4f
KHORSI Yacine <yacine.khorsi@soprasteria.com>
2025-05-05 10:52:46 +0200

    First commit - aigen project

---
+++ src/main/java/com/example/aigen/aigen_demo/AigenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.aigen.aigen_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AigenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AigenDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aigen/aigen_demo/AigenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aigen.aigen_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AigenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
